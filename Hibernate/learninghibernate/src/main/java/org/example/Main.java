436353f013f8c9f449acc7b3da19b309afea7424
Raghav Mundhara <mundhararaghav16@gmail.com>
2026-06-09 22:41:08 +0530

    Hibernate Lesson-1

---
+++ Hibernate/learninghibernate/src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.TimeZone;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+
+	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+		Student student = new Student();
+		student.setId(2);
+		student.setName("John Doe");
+		student.setEmail("john.doe@example.com");
+
+		System.out.println(student);
+		Configuration configuration = new Configuration();
+		configuration.configure("hibernate.cfg.xml");
+		configuration.addAnnotatedClass(Student.class);
+
+		SessionFactory sessionfactory = configuration.buildSessionFactory();
+
+		Session session = sessionfactory.openSession();
+
+		session.beginTransaction();
+		session.persist(student);
+		session.getTransaction().commit();
+		session.close();
+
+	}
+
+}
