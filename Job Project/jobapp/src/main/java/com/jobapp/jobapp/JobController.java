3d20a4f1beaf9696f20a0e5c18d722f7c41da945
Raghav Mundhara <mundhararaghav16@gmail.com>
2026-06-15 20:34:07 +0530

    Updated controller to REST APIs

--- Job Project/jobapp/src/main/java/com/jobapp/jobapp/JobController.java
+++ Job Project/jobapp/src/main/java/com/jobapp/jobapp/JobController.java
@@ -3,37 +3,25 @@ package com.jobapp.jobapp;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.jobapp.jobapp.model.JobPost;
 import com.jobapp.jobapp.service.JobService;
 
+@Controller
 public class JobController {
     @Autowired
     private JobService jobService;
 
-    @GetMapping({"/","/home"})
-    public String home() {
-        return "home";
+    @GetMapping("/jobposts")
+    @ResponseBody
+    public List<JobPost> getAllJobPosts(Model model) {
+        return jobService.getAllJobs();
     }
 
-    @GetMapping("addjob")
-    public String addJob() {
-        return "addjob";
-    }
 
-    @PostMapping("handleForm")
-    public String handleForm(JobPost jobPost) {
-        jobService.addJob(jobPost);
-        return "success";
-    }
-
-    @GetMapping("viewalljobs")
-    public String viewAllJobs(Model m){
-        List<JobPost> jobs= jobService.getAllJobs();
-        m.addAttribute("jobs", jobs);
-        return "viewalljobs";
-    }
 }
