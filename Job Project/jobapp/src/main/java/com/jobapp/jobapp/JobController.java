fc6a2ace4473f95c69e40cc38d8936c2c846b8d4
Raghav Mundhara <mundhararaghav16@gmail.com>
2026-06-16 10:21:19 +0530

    Added CORS and RestController

--- Job Project/jobapp/src/main/java/com/jobapp/jobapp/JobController.java
+++ Job Project/jobapp/src/main/java/com/jobapp/jobapp/JobController.java
@@ -3,30 +3,30 @@ package com.jobapp.jobapp;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.jobapp.jobapp.model.JobPost;
 import com.jobapp.jobapp.service.JobService;
 
-@Controller
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class JobController {
     @Autowired
     private JobService jobService;
 
     @PostMapping("/addjob")
-    @ResponseBody
     public String addJobPost(@RequestBody JobPost jobPost) {
+        System.out.println("Received job post: " + jobPost);
         jobService.addJob(jobPost);
         return "Job post added successfully!";
     }
 
     @GetMapping("/jobposts")
-    @ResponseBody
     public List<JobPost> getAllJobPosts(Model model) {
         System.out.println("Fetching all job posts...");
         return jobService.getAllJobs();
