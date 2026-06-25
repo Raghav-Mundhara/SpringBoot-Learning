6885a51e51cbc9bb04bc4b5f883b4f54036e0605
Raghav Mundhara <mundhararaghav16@gmail.com>
2026-06-25 22:59:37 +0530

    Added UserDetailsService

--- Spring Security/demo/src/main/java/com/springsecurity/demo/config/SecurityConfig.java
+++ Spring Security/demo/src/main/java/com/springsecurity/demo/config/SecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,5 +23,12 @@ public class SecurityConfig {
         http.httpBasic(Customizer.withDefaults());
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withDefaultPasswordEncoder().username("Raghav").password("1234").roles("USER").build();
+        UserDetails admin = User.withDefaultPasswordEncoder().username("RaghavAdmin").password("1234").roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user,admin);
     }
 }
