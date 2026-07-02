76a65acc6d6c86d7577a8542183e2f65a33c8226
Raghav Mundhara <mundhararaghav16@gmail.com>
2026-07-02 18:21:20 +0530

    Created Repository & Service layer for users

--- Practice/practice/src/main/java/com/example/practice/repository/UserRepository.java
+++ Practice/practice/src/main/java/com/example/practice/repository/UserRepository.java
@@ -1,8 +1,20 @@
 package com.example.practice.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRepository {
+import com.example.practice.dto.UserDto;
 
+@Repository
+public class UserRepository {
+    List<UserDto> users = new ArrayList<>();
+
+    public UserRepository(){
+        users.add(new UserDto(1L, "Raghav", "Raghav@gmail.com"));
+        users.add(new UserDto(2L, "Aryan", "Aryan@gmail.com"));
+        users.add(new UserDto(3L, "Anish", "Anish@gmail.com"));
+    }
+    
 }
--- Practice/practice/src/main/java/com/example/practice/service/UserService.java
+++ Practice/practice/src/main/java/com/example/practice/service/UserService.java
@@ -1,8 +1,17 @@
 package com.example.practice.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.practice.repository.UserRepository;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
 
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+    
+    
 }
